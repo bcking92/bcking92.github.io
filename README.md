d7bd64e9fb2eab20c2f5c266bca69a06b9f42e96
Kim_ByungChul <bcking92@gmail.com>
2019-11-14 16:38:49 +0900

    jekyll start

--- README.md
+++
@@ -1,2 +0,0 @@
-# KIM BYUNGCHUL
-
--- js/KBC.js
+++
@@ -1,7 +0,0 @@
-const circle = document.querySelector('#circle')
-
-console.log(circle)
-// circle.addEventListener('click', e => {
-//     console.log('123')
-    
-// })
